d3406257a11b7ee2e91e9da9934d3339f4b951af
Slavik Romanuk <net.highlander@gmail.com>
2011-05-03 01:37:55 +0300

    Dividing code to a few functions for simplier usage and debugging.

--- pyedf.py
+++ pyedf.py
@@ -19,9 +19,18 @@ import edfutils
 
 def read_edf_file(fileobj):
     data = fileobj.read()
-    header = {}
+    
+    header = read_header(data)
+    records_header = read_records_header(data)
+    data_records = read_data_records(data)
+    
+    return {'header': header, 'records_header': records_header}
+    
+    
+def read_header(data):
     # Parse header information based on the EDF/EDF+ specs
     # http://www.edfplus.info/specs/index.html
+    header = {}
     header['version'] = edfutils.parse_int(data[0:7].strip())
     header['patient_id'] = data[7:88].strip()
     header['rec_id'] = data[88:168].strip()
@@ -31,19 +40,19 @@ def read_edf_file(fileobj):
     header['num_items'] = edfutils.parse_int(data[236:244].strip())
     header['data_duration'] = edfutils.parse_float(data[244:252].strip())
     header['num_signals'] = edfutils.parse_int(data[252:256].strip())
-    
-    # more data 256 chars down. in header, but ignoring for now
-    records = []
-    records_a = records.append
-    #rec_pos = 512
+    return header
+
+
+def read_records_header(data):
+    records_header = []
+    records_a = records_header.append
+    rec_pos = 256
     rec_size = 36
-    rec_pos = header['header_bytes']
     
     # skip first rec
     rec_pos += rec_size
     for i in range(header['num_items']-1):
         record = {}
-        print data[rec_pos:rec_pos+rec_size]
         record_split = data[rec_pos:rec_pos+rec_size].split('\x14')
         matches = re.findall(r'([\d]+)', record_split[2])
         record['type'] = record_split[3].strip()
@@ -53,7 +62,11 @@ def read_edf_file(fileobj):
         }
         records_a(record)
         rec_pos += rec_size
-    return {'header': header, 'records': records}
+    return records_header
+
+
+def read_data_records(data):
+    pass
 
 
 def main():
